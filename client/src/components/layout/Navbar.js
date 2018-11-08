ae67f5430d9e5ae42b6c7cd56486e87f5351a807
Elin Jakobsson <elin-jakobsson@hotmail.com>
2018-11-08 11:32:27 +0100

    merging updated navbar to dev

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,7 +8,6 @@ import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import { clearCurrentProfile } from "../../actions/profileActions";
 
-
 const tempMenuStyle = {
   backgroundColor: '#470877',
   borderRadius: '0',
@@ -145,59 +144,6 @@ class Navbar extends Component {
   }
 }
 
-// class Navbar extends Component {
- 
-//   render() {
-
-//     const authLinks = (
-//       <ul>
-//         <li>
-//           <Link to="/mainpage">Biljetter</Link>
-//         </li>
-//         <li>
-//           <Link to="/profile">Profil</Link>
-//         </li>
-//         <li>
-//           <Link to="/mainpage">Filmer och trailers</Link>
-//         </li>
-//         <li>
-//           <Link to="/mainpage">Om oss</Link>
-//         </li>
-//         <li>
-//           <Link to="/mainpage">Kontakta oss</Link>
-//         </li>
-//         <li>
-//           <a href="/" onClick={ event => this.onLogoutClick(event)}>
-//             Logout
-//           </a>
-//         </li>
-//       </ul>
-//     );
-//     const guestLinks = (
-//       <ul>
-//         <li>
-//           <Link to="/login">Logga in</Link>
-//         </li>
-//         <li>
-//           <Link to="/register">Bli medlem</Link>
-//         </li>
-//       </ul>
-//     );
-
-//     return (
-//       <div>
-//         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-//           <div className="container">
-//             <Link className="navbar-brand" to="/">
-//               Kråkebackens biograf
-//             </Link>
-//             {isAuthenticated ? authLinks : guestLinks}
-//           </div>
-//         </nav>
-//       </div>
-//     );
-//   }
-// }
 
 Navbar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
