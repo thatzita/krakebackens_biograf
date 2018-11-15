39648d7f464f59b2c27348c94876baa8cdf2e691
thatzita <robinudehn@gmail.com>
2018-11-15 16:31:25 +0100

    styling for admin

--- client/src/components/admin/monMovies/CreateMonMovie.js
+++ client/src/components/admin/monMovies/CreateMonMovie.js
@@ -1,123 +1,136 @@
 import React, { Component } from "react";
 // import { Segment, Input, Table, Header, Image, Icon } from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import { getAllMovies } from '../../../actions/movieActions';
-import { postMonmovie } from '../../../actions/monMovieActions';
+import { connect } from "react-redux";
+import { getAllMovies } from "../../../actions/movieActions";
+import { postMonmovie } from "../../../actions/monMovieActions";
 
-import DateTimePicker from './DateTimePicker';
-import MoviePicker from './MoviePicker';
-import PreviewSubmitMonMovie from './PreviewSubmitMonMovie';
+import DateTimePicker from "./DateTimePicker";
+import MoviePicker from "./MoviePicker";
+import PreviewSubmitMonMovie from "./PreviewSubmitMonMovie";
+import Admin from "../../admin/Admin";
+import "./monMovies.css";
 
 class CreateMonMovie extends Component {
-    constructor(){
-        super();
-        this.state = {
-            search: '',
-            date: '',
-            time: '',
-            movieId: '',
-            eventObject: {},
-            previewPage: false 
-        }
+  constructor() {
+    super();
+    this.state = {
+      search: "",
+      date: "",
+      time: "",
+      movieId: "",
+      eventObject: {},
+      previewPage: false
+    };
+  }
+
+  componentDidMount() {
+    this.props.getAllMovies();
+  }
+
+  onSubmitEvent = () => {
+    let createDate = new Date(this.state.date + "T" + this.state.time);
+    let utc_time = createDate.toUTCString();
+
+    let monMovieDb = {
+      mov: this.state.eventObject,
+      date: this.state.date,
+      time: this.state.time,
+      utc_time: utc_time
+    };
+    console.log("Value : ", monMovieDb);
+    this.props.postMonmovie(monMovieDb);
+  };
+
+  onSearch = value => {
+    this.setState({ search: value });
+  };
+
+  handleTimeChange = (e, { name, value }) => this.setState({ [name]: value });
+
+  selectMovie = (id, movies) => {
+    let movId;
+    if (id) {
+      if (this.state.movieId === id) {
+        movId = "";
+        this.setState({ movieId: movId, eventObject: {} });
+      } else {
+        movId = id;
+        let objArray = movies.filter(obj => obj._id === movId);
+        this.setState({ movieId: movId, eventObject: objArray[0] || {} });
+      }
     }
+  };
 
-    componentDidMount(){
-        this.props.getAllMovies();
-    }
-    
-    onSubmitEvent = () => {
-        
-        let createDate = new Date(this.state.date + 'T' + this.state.time);
-        let utc_time = createDate.toUTCString();
+  goToOrLeavePreviewPage = bol => {
+    this.setState({ previewPage: bol });
+  };
 
-        let monMovieDb = {
-            mov: this.state.eventObject,
-            date: this.state.date,
-            time: this.state.time,
-            utc_time: utc_time
-        }
-        console.log('Value : ', monMovieDb);
-        this.props.postMonmovie(monMovieDb);
-    }
+  render() {
+    console.log(this.state);
 
-    onSearch = (value) => {
-        this.setState({ search: value})
-    } 
-    
-    handleTimeChange = (e, { name, value }) => this.setState({ [name]: value });
+    let movies = this.props.movies || [];
 
-    selectMovie = (id, movies) => {
-        let movId;
-        if(id){
-            if (this.state.movieId === id) {
-                movId = '';
-                this.setState({movieId: movId, eventObject: {}});
-            }else{
-                movId = id;
-                let objArray = movies.filter(obj => obj._id === movId)
-                this.setState({movieId: movId, eventObject: objArray[0] || {}});
-            } 
-        }  
-    }
+    let movieList =
+      this.state.search.length <= 0
+        ? []
+        : movies.filter(movie => {
+            return (
+              movie.title
+                .toLowerCase()
+                .indexOf(this.state.search.toLowerCase()) !== -1
+            );
+          });
 
-    goToOrLeavePreviewPage = (bol) => {this.setState({previewPage: bol})}
+    const createPage = (
+      <React.Fragment>
+        <Admin />
+        <MoviePicker
+          movieId={this.state.movieId}
+          onSearch={this.onSearch}
+          selectMovie={this.selectMovie}
+          movieList={movieList}
+          movies={movies}
+          eventObject={this.state.eventObject}
+        />
+        <DateTimePicker
+          eventObject={this.state.eventObject}
+          handleTimeChange={this.handleTimeChange}
+          movieId={this.state.movieId}
+          date={this.state.date}
+          time={this.state.time}
+          goToOrLeavePreviewPage={this.goToOrLeavePreviewPage}
+        />
+      </React.Fragment>
+    );
 
-    render(){
-        console.log(this.state);
-        
-        let movies = this.props.movies || [];
+    const previewSubmitPage = (
+      <React.Fragment>
+        <PreviewSubmitMonMovie
+          eventObject={this.state.eventObject}
+          movies={movies}
+          date={this.state.date}
+          time={this.state.time}
+          goToOrLeavePreviewPage={this.goToOrLeavePreviewPage}
+          onSubmitEvent={this.onSubmitEvent}
+        />
+      </React.Fragment>
+    );
 
-        let movieList = (this.state.search.length <= 0) ? [] : movies.filter(
-            (movie) => {
-                return movie.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
-            }
-        ); 
-
-        const createPage = (
-        <React.Fragment>
-            <MoviePicker 
-                movieId={this.state.movieId} 
-                onSearch={this.onSearch} 
-                selectMovie={this.selectMovie} 
-                movieList={movieList} 
-                movies={movies}
-                eventObject={this.state.eventObject} 
-            />
-            <DateTimePicker 
-                eventObject={this.state.eventObject} 
-                handleTimeChange={this.handleTimeChange} 
-                movieId={this.state.movieId} 
-                date={this.state.date} 
-                time={this.state.time}
-                goToOrLeavePreviewPage={this.goToOrLeavePreviewPage} 
-            />
-        </React.Fragment>
-        );
-
-        const previewSubmitPage = (
-        <React.Fragment>
-            <PreviewSubmitMonMovie 
-                eventObject={this.state.eventObject} 
-                movies={movies} 
-                date={this.state.date} 
-                time={this.state.time} 
-                goToOrLeavePreviewPage={this.goToOrLeavePreviewPage}
-                onSubmitEvent={this.onSubmitEvent}
-            />
-        </React.Fragment>
-        )
-       
-        return (
-        <React.Fragment>
-            {this.state.previewPage ? previewSubmitPage : createPage }       
-        </React.Fragment>
-        );
-    }
-        
-} 
+    return (
+      <React.Fragment>
+        <div className="containerMonMovies">
+          {this.state.previewPage ? previewSubmitPage : createPage}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
-    movies: state.movies.movies   
+  movies: state.movies.movies
 });
 
-export default connect(mapStateToProps, {getAllMovies, postMonmovie})(CreateMonMovie);
+export default connect(
+  mapStateToProps,
+  { getAllMovies, postMonmovie }
+)(CreateMonMovie);
