a7588a31d7571c46dc42d6799bf6ae7ecd35a8e3
Elin Jakobsson <elin-jakobsson@hotmail.com>
2018-12-28 17:04:21 +0100

    extra styling

--- client/src/components/main/MovieBackdropDisplay.js
+++ client/src/components/main/MovieBackdropDisplay.js
@@ -29,7 +29,8 @@ export default class MovieBackdropDisplay extends Component {
   render() {
     const { activeItem, movieTrailer } = this.state;
     let movieItem = this.props.monMovie || {};
-    let displayImage = movieItem.background || "default.jpg";
+    let displayImage = movieItem.background || "skogdel3.jpg";
+    console.log(movieItem);
 
     if (displayImage === "http://image.tmdb.org/t/p/originalnull") {
       displayImage = "curtain.jpg";
@@ -51,110 +52,118 @@ export default class MovieBackdropDisplay extends Component {
         />
         <div className="backdropContent">
           <div style={{ marginTop: "3rem" }}>
-            <Label color="violet">På bio nu</Label>
+            {Object.keys(movieItem).length !== 0 ? (
+              <Label color="violet">På bio nu</Label>
+            ) : null}
 
-            <h1>{movieItem.title ? movieItem.title : "Title"}</h1>
+            <h1>
+              {movieItem.title
+                ? movieItem.title
+                : "Välkommen till Kråkebackens Biograf"}
+            </h1>
             <span>
-              {movieItem.screeningDate ? movieItem.screeningDate : "1989-03-08"}
+              {movieItem.screeningDate ? movieItem.screeningDate : ""}
             </span>
             <span style={{ marginLeft: "1rem" }}>
-              {movieItem.screeningTime ? movieItem.screeningTime : "13:37"}
+              {movieItem.screeningTime ? movieItem.screeningTime : ""}
             </span>
-            <Menu
-              inverted
-              secondary
-              style={{
-                width: "220px",
-                margin: "auto",
-                boxShadow: "none",
-                backgroundColor: "rgba(0,0,0,0)",
-                border: "0"
-              }}
-            >
-              <Modal
-                style={{ textAlign: "center", backgroundColor: "black" }}
-                trigger={
-                  <Menu inverted secondary style={{ background: "none" }}>
-                    <Menu.Item
-                      name="trailer"
-                      active={activeItem === "trailer"}
-                      onClick={this.handleItemClick}
-                    >
-                      <Icon name="play" /> Spela trailer
-                    </Menu.Item>
-                  </Menu>
-                }
-                open={this.state.modalOpen}
-                onClose={this.handleClose}
+            {Object.keys(movieItem).length !== 0 ? (
+              <Menu
+                inverted
+                secondary
+                style={{
+                  width: "220px",
+                  margin: "auto",
+                  boxShadow: "none",
+                  backgroundColor: "rgba(0,0,0,0)",
+                  border: "0"
+                }}
               >
-                {
-                  <React.Fragment>
-                    <Icon
-                      style={{
-                        position: "absolute",
-                        right: "-2.5rem",
-                        fontSize: "2rem",
-                        fontWeight: "100"
-                      }}
-                      color="red"
-                      onClick={this.handleClose}
-                      inverted
-                      name="close"
-                    />
-
-                    {movieTrailer === "//youtube.com" ? (
-                      <div
-                        style={{
-                          background: "black",
-                          height: "400px",
-                          border: "2px solid white"
-                        }}
+                <Modal
+                  style={{ textAlign: "center", backgroundColor: "black" }}
+                  trigger={
+                    <Menu inverted secondary style={{ background: "none" }}>
+                      <Menu.Item
+                        name="trailer"
+                        active={activeItem === "trailer"}
+                        onClick={this.handleItemClick}
                       >
-                        <Image
+                        <Icon name="play" /> Spela trailer
+                      </Menu.Item>
+                    </Menu>
+                  }
+                  open={this.state.modalOpen}
+                  onClose={this.handleClose}
+                >
+                  {
+                    <React.Fragment>
+                      <Icon
+                        style={{
+                          position: "absolute",
+                          right: "-2.5rem",
+                          fontSize: "2rem",
+                          fontWeight: "100"
+                        }}
+                        color="red"
+                        onClick={this.handleClose}
+                        inverted
+                        name="close"
+                      />
+
+                      {movieTrailer === "//youtube.com" ? (
+                        <div
                           style={{
-                            width: "300px",
-                            top: "18%",
-                            margin: "0 auto"
-                          }}
-                          src="krakebackens_logo.png"
-                        />
-                        <p
-                          style={{
-                            color: "white",
-                            fontSize: "3rem",
-                            textAlign: "center",
-                            position: "relative",
-                            top: "-21rem"
+                            background: "black",
+                            height: "400px",
+                            border: "2px solid white"
                           }}
                         >
-                          Filmen saknar trailer
-                        </p>
-                      </div>
-                    ) : (
-                      <iframe
-                        title="movie trailer"
-                        width="900"
-                        height="600"
-                        src={movieTrailer}
-                      />
-                    )}
-                  </React.Fragment>
-                }
-              </Modal>
+                          <Image
+                            style={{
+                              width: "300px",
+                              top: "18%",
+                              margin: "0 auto"
+                            }}
+                            src="krakebackens_logo.png"
+                          />
+                          <p
+                            style={{
+                              color: "white",
+                              fontSize: "3rem",
+                              textAlign: "center",
+                              position: "relative",
+                              top: "-21rem"
+                            }}
+                          >
+                            Filmen saknar trailer
+                          </p>
+                        </div>
+                      ) : (
+                        <iframe
+                          title="movie trailer"
+                          width="900"
+                          height="600"
+                          src={movieTrailer}
+                        />
+                      )}
+                    </React.Fragment>
+                  }
+                </Modal>
 
-              <Menu.Item
-                as={Link}
-                to={{
-                  pathname: "/movieselection",
-                  state: { movieId: movieItem._id }
-                }}
-                name="biljetter"
-                active={activeItem === "biljetter"}
-                onClick={this.handleItemClick}
-              >
-                <Icon name="ticket" /> Biljetter
-              </Menu.Item>
-            </Menu>
+                <Menu.Item
+                  as={Link}
+                  to={{
+                    pathname: "/movieselection",
+                    state: { movieId: movieItem._id }
+                  }}
+                  name="biljetter"
+                  active={activeItem === "biljetter"}
+                  onClick={this.handleItemClick}
+                >
+                  <Icon name="ticket" /> Biljetter
+                </Menu.Item>
+              </Menu>
+            ) : null}
           </div>
         </div>
       </div>
