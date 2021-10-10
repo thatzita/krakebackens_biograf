78c3e933a4e36a1183f50a72a581b63ab6b89c93
thatzita <robinudehn@gmail.com>
2021-10-10 11:19:24 +0200

    fixed bug with release date when searching for movie

--- client/src/components/movies/AddMovie.js
+++ client/src/components/movies/AddMovie.js
@@ -140,7 +140,7 @@ class AddMovie extends Component {
             />
             <Item.Content>
               <Item.Header>
-                {movie.title} ( {movie.release_date.substring(0, 4)} ){" "}
+                {movie.title} ( {movie.release_date ? movie.release_date.substring(0, 4) : "" } ){" "}
               </Item.Header>
               <Item.Extra>&nbsp;</Item.Extra>
               <Button.Group className="addMovieBtnGroup">
